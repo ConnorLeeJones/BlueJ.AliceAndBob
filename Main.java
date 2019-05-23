1963b826df11fac0fb2105a31030e25b5e3e307e
Connor Jones <ConnorLeeJones@gmail.com>
2019-05-23 19:15:12 -0400

    Completed lab

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = scnr.next();
 
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            System.out.println("Hello, " + name + "!"); 
+        } else {
+            System.out.println("You aren't Alice or Bob... go away!");
+        }
     }
 }
